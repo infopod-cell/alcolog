fab7d9159f1e8d9d75d55e547edd0de200dce779
infopod-cell <infopod@ya.ru>
2026-09-07 23:04:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -632,3 +632,18 @@ function render() {
     renderDayDetails();
     renderAnalytics();
 }
+// Автообновление при появлении новой версии
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(reg => {
+    reg.addEventListener('updatefound', () => {
+      const newWorker = reg.installing;
+      if (newWorker) {
+        newWorker.addEventListener('statechange', () => {
+          if (newWorker.state === 'activated') {
+            window.location.reload();
+          }
+        });
+      }
+    });
+  });
+}
